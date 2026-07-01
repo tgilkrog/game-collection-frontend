d5717c09a7c37523268b16fb0f22e7292420f8e7
tg-aveo <tgilkrog@gmail.com>
2026-07-01 10:19:00 +0200

    change css to use variables

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,6 @@ import "./index.css";
 import "./styles/base.css";
 import "./styles/ui.css";
 import "./styles/variables.css";
-import "./styles/gameList.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
